17eb4295f0fc981adec8d161900fb76492078866
Wim Haanstra <wim@wim.me>
2015-12-20 08:29:48 +0100

    Initial Commit

---
+++ pixelbits/pixelbits.h
@@ -0,0 +1,19 @@
+//
+//  pixelbits.h
+//  pixelbits
+//
+//  Created by Wim Haanstra on 20/12/15.
+//  Copyright © 2015 Wim Haanstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pixelbits.
+FOUNDATION_EXPORT double pixelbitsVersionNumber;
+
+//! Project version string for pixelbits.
+FOUNDATION_EXPORT const unsigned char pixelbitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pixelbits/PublicHeader.h>
+
+
